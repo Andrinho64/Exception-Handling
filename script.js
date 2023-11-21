82f6a7728d95e78f752ba7a831ad700049a4bc7e
Andrew Caetano <andrewr_caetano@hotmail.com>
2023-11-21 18:11:56 -0300

    [Andrew Caetano] Exception Handling

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-function calculateSum() {
-  const value1 = document.getElementById('value1').value;
-  const value2 = document.getElementById('value2').value;
-  const result = Number(value1) + Number(value2);
+function isNumeric(value) {
+  return !Number.isNaN(Number(value));
+}
 
-  return result;
+function validateInput(value1, value2) {
+  if (!value1 || !value2) {
+    throw new Error('Preencha os campos para realizar a soma');
+  }
+
+  if (!isNumeric(value1) || !isNumeric(value2)) {
+    throw new Error('Informe dois números para realizar a soma');
+  }
+}
+
+function calculateSum() {
+  try {
+    const value1 = document.getElementById('value1').value;
+    const value2 = document.getElementById('value2').value;
+
+    validateInput(value1, value2);
+
+    const result = Number(value1) + Number(value2);
+
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+
+    return result;
+  } catch (error) {
+    document.getElementById('result').innerHTML = error.message;
+    throw error;
+  }
 }
 
 function displayResult(result) {
@@ -11,8 +36,14 @@ function displayResult(result) {
 }
 
 function sum() {
-  const result = calculateSum();
-  displayResult(result);
+  try {
+    const result = calculateSum();
+    if (!Number.isNaN(result)) {
+      displayResult(result);
+    }
+  } catch (error) {
+    // Captura o erro lançado pela função calculateSum durante os testes
+  }
 }
 
 window.onload = () => {
